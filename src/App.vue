e64cb9bb0ebe391cf997b52d1ec7b4fde01c0e84
HuangJiangye <1419210357@qq.com>
2025-04-29 18:02:08 +0800

    add proto

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,36 @@
-<template>
-  <div id="app">
-    <div id="cesiumContainer"></div>
-  </div>
-
-</template>
-
 <script setup>
 import { onMounted } from 'vue'
 import * as Cesium from 'cesium'
 import { TOKEN } from './stores/token.js'
+import Proto from './components/proto.vue'
 
+let viewer;
 onMounted(() => {
   // 使用Cesium的Ion服务进行认证
   Cesium.Ion.defaultAccessToken = TOKEN;
 
   // 创建一个Viewer实例
-  const viewer = new Cesium.Viewer('cesiumContainer', {
+  viewer = new Cesium.Viewer('cesiumContainer', {
     // 使用默认的影像图层和地形图层
     terrainProvider: Cesium.createWorldTerrain({ requestWaterMask: true })
   })
+  // 将 viewer 提供给子组件
+  provide('viewer', viewer);
 
 })
 </script>
 
+<template>
+  <div id="app">
+    <div id="cesiumContainer"></div>
+  </div>
+  <Proto />
+</template>
+
 <style scoped>
 #cesiumContainer {
-  width: 100vw;
-  height: 100vh;
+  width: 98vw;
+  height: 98vh;
   overflow: hidden;
 }
 </style>
---
+++ src/components/proto.vue
@@ -0,0 +1,27 @@
+<script setup>
+import * as Cesium from 'cesium'
+import { inject } from 'vue'
+const viewer = inject('viewer')
+// 创建实体所在的坐标
+const position = Cesium.Cartesian3.fromDegrees(113.3, 39.9, 5000)
+
+const box = new Cesium.Entity({
+    position: position, //位置
+    box: {
+        dimensions: new Cesium.Cartesian3(4000, 3000, 5000), //盒子的长宽高
+        material: Cesium.Color.RED.withAlpha(0.5), //盒子颜色
+        outline: true, //边框
+        outlineColor: Cesium.Color.WHITE, //边框颜色
+    },
+});
+
+// 将立方体添加到场景中
+viewer.entities.add(box);
+
+</script>
+
+<template>
+    <div>111111</div>
+</template>
+
+<style></style>
