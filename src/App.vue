deb977586ef3efee9454b655cf08744b0d133c67
gaga0714 <140368447+gaga0714@users.noreply.github.com>
2025-05-01 16:34:18 +0800

    Revert "Gaga"

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,32 @@
-<script setup>
-import { onMounted, provide, ref } from 'vue'
-import * as Cesium from 'cesium'
-import { TOKEN } from './stores/token.js'
-import Proto from './components/proto.vue'
-import Flyer from './components/flyer.vue'
-import Connector from './components/connector.vue'
-const viewerReady = ref(false);
-let viewer;
-onMounted(async () => {
-  // 使用Cesium的Ion服务进行认证
-  Cesium.Ion.defaultAccessToken = TOKEN;
-
-  // 创建一个Viewer实例
-  viewer = new Cesium.Viewer('cesiumContainer', {
-    // 使用全球地形数据，启用水面反射和水下地形渲染
-    terrainProvider: Cesium.createWorldTerrain({ requestWaterMask: true }),
-    infoBox: true,
-  })
-
-  provide('viewer', viewer);
-  viewerReady.value = true;
-})
-</script>
-
 <template>
   <div id="app">
     <div id="cesiumContainer"></div>
   </div>
-  <Proto v-if="viewerReady" />
-  <Flyer v-if="viewerReady" />
-  <Connector v-if="viewerReady" />
+
 </template>
+
+<script setup>
+import { onMounted } from 'vue'
+import * as Cesium from 'cesium'
+import { TOKEN } from './stores/token.js'
+
+onMounted(() => {
+  // 使用Cesium的Ion服务进行认证
+  Cesium.Ion.defaultAccessToken = TOKEN;
+
+  // 创建一个Viewer实例
+  const viewer = new Cesium.Viewer('cesiumContainer', {
+    // 使用默认的影像图层和地形图层
+    terrainProvider: Cesium.createWorldTerrain({ requestWaterMask: true })
+  })
+
+})
+</script>
 
 <style scoped>
 #cesiumContainer {
-  width: 98vw;
-  height: 98vh;
+  width: 100vw;
+  height: 100vh;
   overflow: hidden;
 }
 </style>
